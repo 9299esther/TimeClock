e1c6db90691d7b20d9ce99cc885af3c9e2954556
Esther Schoemann <9299esther@gmail.com>
2021-03-03 15:33:10 +0200

    Update Create Database.sql

--- Create Database.sql
+++ Create Database.sql
@@ -1,4 +1,5 @@
 create database timeclock;
+use timeclock;
 create table user( id int not null auto_increment, name varchar (50), date_created datetime, primary key (id));
 
 INSERT INTO user (name, date_created) VALUES ('Esther Sasson', now());
