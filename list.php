819a5d32a1675134f8bec864f1b100c3f9eb6c0a
9299esther <9299esther@gmail.com>
2021-03-03 12:55:23 +0000

    list file

---
+++ list.php
@@ -0,0 +1,24 @@
+<?php
+echo "Hello world";
+
+// Connect to datase
+//
+$conn=new mysqli("localhost", "Esther", "Esther","Esther1");
+
+// Check connection
+if($conn->connect_error){
+	die("Connection failed: ". $conn->connect_error);
+}
+echo "<br>Connected successfully<br>";
+
+$result = $conn->query("select * from user");
+
+if ($result->num_rows > 0) {
+	// output data of each row
+	while($row=$result->fetch_assoc()) {
+		echo "id: <b>".$row["id"]."</b> - Name:<b> " . $row["name"]. "</b> - Date Created: " . $row["date_created"]. "<br>";
+	}
+} else {
+	echo "0 results";
+}
+?>
